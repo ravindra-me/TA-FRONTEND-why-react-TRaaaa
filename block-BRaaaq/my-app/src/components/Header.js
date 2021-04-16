b1eba75cbdecbf17b5f8f6a46fab2a7932c3cc32
ravidra-me <ravindrarajpoot9628172@gmail.com>
2021-04-16 18:05:41 +0530

    prop-types

---
+++ block-BRaaaq/my-app/src/components/Header.js
@@ -0,0 +1,26 @@
+function Header() {
+  return (
+    <header>
+      <div className="container flex justify-between align-center">
+        <a href="" className="logo">
+          logo
+        </a>
+        <nav>
+          <ul className="flex align-center ">
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
